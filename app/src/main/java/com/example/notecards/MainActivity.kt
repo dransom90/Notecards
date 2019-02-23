e55447199f460d5230ca985b54c01552c474edcb
DJ Ransom <dransom90@yahoo.com>
2019-02-22 17:42:33 -0700

    Added a date to the notecard and a poor attempt at sorting the displayed tasks.

--- app/src/main/java/com/example/notecards/MainActivity.kt
+++ app/src/main/java/com/example/notecards/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.notecards
 
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
@@ -10,7 +8,6 @@ import android.widget.ArrayAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import kotlinx.android.synthetic.main.content_view_notecards.view.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,9 +25,10 @@ class MainActivity : AppCompatActivity() {
         noteFrequencySpinner.adapter = frequencyAdapter
 
         saveBtn.setOnClickListener {
-            val date = calculateDueDate()
-            val notecard = Notecard(titleTxt.text.toString(), noteFrequencySpinner.selectedItem.toString(), date)
+            val notecard = Notecard(titleTxt.text.toString(), noteFrequencySpinner.selectedItem.toString())
+            notecard.calculateDueDate()
             DataManager.notes.add(notecard)
+            DataManager.notes.sortedWith(compareBy {it.nextDueDate})
             finish()
         }
     }
@@ -49,9 +47,5 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    private fun calculateDueDate() : Date {
-        return Date(1,1,1)
     }
 }
--- app/src/main/java/com/example/notecards/Notecard.kt
+++ app/src/main/java/com/example/notecards/Notecard.kt
@@ -1,7 +1,39 @@
 package com.example.notecards
 
 import java.util.*
+import java.time.*
+import java.time.temporal.TemporalAdjusters
 
-data class Notecard (var title: String, var frequency: String, var nextDueDate: Date){
+data class Notecard (var title: String, var frequency: String){
     override fun toString(): String = "$frequency: $title"
+
+    var nextDueDate = LocalDate.now()
+
+    fun calculateDueDate(){
+
+        if(frequency == "Daily")
+        {
+            val dt = Date ()
+            nextDueDate = nextDueDate.plusDays(1)
+            return
+        }
+
+        if(frequency == "Monday" || frequency == "Tuesday" || frequency == "Wednesday" || frequency == "Thursday" ||
+        frequency == "Friday" || frequency == "Saturday" || frequency == "Sunday")
+        {
+            getNextDayOfWeek(DayOfWeek.valueOf(frequency.toUpperCase()))
+        }
+
+        if(frequency == "January" || frequency == "February" || frequency == "March" || frequency == "April" ||
+                frequency == "May" || frequency == "June" || frequency == "July" || frequency == "August" ||
+                frequency == "September" || frequency == "October" || frequency == "November" || frequency == "December")
+        {
+            nextDueDate = nextDueDate.plusMonths(12)
+        }
+    }
+
+    fun getNextDayOfWeek(weekDay: DayOfWeek)
+    {
+        nextDueDate = nextDueDate.with(TemporalAdjusters.next(weekDay))
+    }
 }
